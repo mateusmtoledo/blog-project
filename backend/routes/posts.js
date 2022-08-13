3696572f77ab66d849bf624833f0e23ff7772ddb
Mateus Mauricio Toledo <mateusmtoledo1@gmail.com>
2022-08-13 18:30:54 -0300

    Add get, put and delete methods for individual posts

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -12,7 +12,6 @@ router.get('/', (req, res, next) => {
       next(err);
       return;
     }
-    console.log(req.isAuthenticated());
     res.json(posts);
   });
 });
@@ -30,7 +29,76 @@ router.post('/', [
         status: 400,
         errors,
       });
+      return;
     }
+
+    const { title, text } = req.body;
+    new Post({
+      title,
+      text,
+      author: req.user._id,
+    }).save((err, post) => {
+      if (err) {
+        next(err);
+        return;
+      }
+      res.json(post);
+    });
+  },
+]);
+
+router.get('/:postId', (req, res, next) => {
+  const { postId } = req.params;
+  Post.findById(postId, (err, post) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.json(post);
+  });
+});
+
+router.put('/:postId', [
+  passport.authenticate('jwt', { session: false }),
+  isAdmin,
+  body('title', 'Title must have 3 to 64 characters').trim().isLength({ min: 3, max: 64 }).escape(),
+  body('text', 'Text is required').trim().isLength({ min: 1 }).escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      next({
+        status: 400,
+        errors,
+      });
+      return;
+    }
+
+    const { title, text } = req.body;
+    const { postId } = req.params;
+    Post.findByIdAndUpdate(postId, { title, text }, (err, post) => {
+      if (err) {
+        next(err);
+        return;
+      }
+      res.json(post);
+    });
+  },
+]);
+
+router.delete('/:postId', [
+  passport.authenticate('jwt', { session: false }),
+  isAdmin,
+
+  (req, res, next) => {
+    const { postId } = req.params;
+    Post.findByIdAndDelete(postId, (err, post) => {
+      if (err) {
+        next(err);
+        return;
+      }
+      res.json(post);
+    });
   },
 ]);
 
