a369f0fdb439a0607b4e044b89dcc56baf05ecfd
Mateus Toledo <mateusmtoledo1@gmail.com>
2022-08-16 17:19:51 -0300

    Authenticate user on page load if there is a token

--- frontend/blog/src/App.js
+++ frontend/blog/src/App.js
@@ -1,10 +1,28 @@
 import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { UserContext } from "./components/UserContext";
+import api from "./api";
 
 function App() {
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    async function getUser() {
+      if(localStorage.getItem('token')) {
+        try {
+          const response = await api.get('/userdata');
+          const { data } = response;
+          setUser(data.user);
+        } catch(err) {
+          localStorage.setItem('token', '');
+          console.log(err);
+        }
+      }
+    }
+    getUser();
+  }, []);
+
   return (
     <>
       <UserContext.Provider value={{user, setUser}}>
--- frontend/blog/src/api.js
+++ frontend/blog/src/api.js
@@ -4,4 +4,9 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
+api.interceptors.request.use((config) => {
+  config.headers.Authorization = 'Bearer ' + localStorage.getItem('token');
+  return config;
+});
+
 export default api;
