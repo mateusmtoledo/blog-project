f232225f1c929d17741fa4334afe12c2c97a5a62
Mateus Toledo <mateusmtoledo1@gmail.com>
2022-08-16 15:27:06 -0300

    Remove link from author name and date

--- frontend/blog/src/components/PostOverview.js
+++ frontend/blog/src/components/PostOverview.js
@@ -5,9 +5,9 @@ function PostOverview({ post }) {
     <div>
       <Link to={`/${post._id}`}>
         <h3>{post.title}</h3>
-        <p>Posted by <strong>{post.author.fullName}</strong></p>
-        <p>{new Date(post.date).toLocaleString('en-US')}</p>
       </Link>
+      <p>Posted by <strong>{post.author.fullName}</strong></p>
+      <p>{new Date(post.date).toLocaleString('en-US')}</p>
       {
         post.text.split('\n').map((paragraph, i) => (
           <p key={i}>{paragraph}</p>
